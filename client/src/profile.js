1b74187959fe356d63c92df253147858b40abc92
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-08-27 15:02:34 -0500

    made the add-service button and converted all html select options to load using js

--- client/src/profile.js
+++ client/src/profile.js
@@ -5,15 +5,23 @@ import './scss/styles.scss';
 import * as bootstrap from 'bootstrap';
 
 import UploadIcon from './images/upload.png';
+import PlusIcon from './images/plus.png';
 import PeaceChicken from './images/peace_chicken.jpg';
+import { sectors, countries } from './constants/profile';
 
 const pictureInput = document.getElementById('profile-picture');
 const picturePreview = document.getElementById('pic-preview');
 const uploadIcon = document.getElementById('upload-icon');
-const yearSelect = document.getElementById('year');
+const plusIcon = document.getElementById('plus-icon');
+const firstCountrySelect = document.getElementById('country');
+const firstSectorSelect = document.getElementById('sector');
+const firstYearSelect = document.getElementById('year');
+const addServiceButton = document.getElementById('add-service');
+const serviceSection = document.getElementById('service-section');
 
 picturePreview.src = PeaceChicken;
 uploadIcon.src = UploadIcon;
+plusIcon.src = PlusIcon;
 
 const showPreview = () => {
   // Get the selected file
@@ -35,16 +43,106 @@ const showPreview = () => {
   reader.readAsDataURL(file);
 };
 
-const loadYears = () => {
-  let year = new Date().getFullYear();
+const loadCountries = (countrySelectElement) => {
+  let option = document.createElement('option');
+  option.selected = true;
+  option.innerHTML = 'Select';
+  countrySelectElement.appendChild(option);
+  countries.forEach((country) => {
+    option = document.createElement('option');
+    option.value = country;
+    option.innerHTML = country;
+    countrySelectElement.appendChild(option);
+  })
+};
+
+const loadSectors = (sectorSelectElement) => {
+  let option = document.createElement('option');
+  option.selected = true;
+  option.innerHTML = 'Select';
+  sectorSelectElement.appendChild(option);
+  sectors.forEach((sector) => {
+    option = document.createElement('option');
+    option.value = sector;
+    option.innerHTML = sector;
+    sectorSelectElement.appendChild(option);
+  });
+};
+
+const loadYears = (yearSelectElement) => {
+  let option = document.createElement('option');
+  option.selected = true;
+  option.innerHTML = 'Select';
+  yearSelectElement.appendChild(option);
+  let year = new Date().getFullYear(); //current year
   while (year >= 1961) {
-    let nextOption = document.createElement('option');
-    nextOption.value = year;
-    nextOption.innerHTML = year;
-    yearSelect.appendChild(nextOption);
+    option = document.createElement('option');
+    option.value = year;
+    option.innerHTML = year;
+    yearSelectElement.appendChild(option);
     year--;
   };
 };
 
+const addService = (event) => {
+  event.preventDefault();
+  const countryDiv = document.createElement('div');
+  countryDiv.classList.add('col');
+  countryDiv.classList.add('col-12');
+  countryDiv.classList.add('col-sm-4');
+  countryDiv.classList.add('mx-auto');
+  serviceSection.appendChild(countryDiv);
+  const countryLabel = document.createElement('label');
+  countryLabel.for = 'country';
+  countryLabel.innerHTML = 'Country';
+  countryDiv.appendChild(countryLabel);
+  const countrySelect = document.createElement('select');
+  countrySelect.classList.add('form-select');
+  countrySelect.classList.add('mb-3');
+  countrySelect.id = 'country';
+  countrySelect.ariaLabel = 'Country where you served';
+  countryDiv.appendChild(countrySelect);
+  loadCountries(countrySelect);
+
+  const sectorDiv = document.createElement('div');
+  sectorDiv.classList.add('col');
+  sectorDiv.classList.add('col-12');
+  sectorDiv.classList.add('col-sm-4');
+  sectorDiv.classList.add('mx-auto');
+  serviceSection.appendChild(sectorDiv);
+  const sectorLabel = document.createElement('label');
+  sectorLabel.for = 'sector';
+  sectorLabel.innerHTML = 'Sector';
+  sectorDiv.appendChild(sectorLabel);
+  const sectorSelect = document.createElement('select');
+  sectorSelect.classList.add('form-select');
+  sectorSelect.classList.add('mb-3');
+  sectorSelect.id = 'sector';
+  sectorSelect.ariaLabel = 'Sector you served';
+  sectorDiv.appendChild(sectorSelect);
+  loadSectors(sectorSelect);
+
+  const yearDiv = document.createElement('div');
+  yearDiv.classList.add('col');
+  yearDiv.classList.add('col-12');
+  yearDiv.classList.add('col-sm-4');
+  yearDiv.classList.add('mx-auto');
+  serviceSection.appendChild(yearDiv);
+  const yearLabel = document.createElement('label');
+  yearLabel.for = 'year';
+  yearLabel.innerHTML = 'Year training started';
+  yearDiv.appendChild(yearLabel);
+  const yearSelect = document.createElement('select');
+  yearSelect.classList.add('form-select');
+  yearSelect.classList.add('mb-3');
+  yearSelect.id = 'year';
+  yearSelect.ariaLabel = 'Year you landed in-country';
+  yearDiv.appendChild(yearSelect);
+  loadYears(yearSelect);
+};
+
 pictureInput.addEventListener('input', showPreview);
-loadYears();
+addServiceButton.addEventListener('click', addService);
+loadCountries(firstCountrySelect);
+loadSectors(firstSectorSelect);
+loadYears(firstYearSelect);
