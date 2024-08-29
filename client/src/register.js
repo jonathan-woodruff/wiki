a8a1594dffe487a5c83d87410cbfd8be3ad89b8e
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-08-29 06:42:19 -0500

    added checkbox to register page

--- client/src/register.js
+++ client/src/register.js
@@ -9,28 +9,34 @@ import { onRegister } from './api/auth';
 const form = document.getElementById('form');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
+const checkboxInput = document.getElementById('checkbox');
 const errorElement = document.getElementById('error-message');
 const loginLink = document.getElementById('login-link');
 
 const registerUser = async (event) => {
   event.preventDefault();
-  try {
-    const credentials = {
-        email: emailInput.value,
-        password: passwordInput.value
+  if (checkboxInput.checked) {
+    try {
+      const credentials = {
+          email: emailInput.value,
+          password: passwordInput.value
+      };
+      await onRegister(credentials);
+      window.location.href = './profile.html';
+    } catch(error) {
+      const errorMessage = error.response.data.errors[0].msg; //error from axios
+      errorElement.innerHTML = errorMessage;
+      errorElement.classList.remove('d-none')
+      if (errorMessage === 'Email already exists' || errorMessage === 'Please enter a valid email address') {
+        emailInput.classList.add('border-danger');
+      } else if (errorMessage === 'Password must be between 6 and 15 characters') {
+        passwordInput.classList.add('border-danger');
+      };
     };
-    await onRegister(credentials);
-    window.location.href = './profile.html';
-  } catch(error) {
-    const errorMessage = error.response.data.errors[0].msg; //error from axios
-    errorElement.innerHTML = errorMessage;
-    errorElement.classList.remove('d-none')
-    if (errorMessage === 'Email already exists' || errorMessage === 'Please enter a valid email address') {
-      emailInput.classList.add('border-danger');
-    } else if (errorMessage === 'Password must be between 6 and 15 characters') {
-      passwordInput.classList.add('border-danger');
-    };
-  };
+  } else {
+    errorElement.innerHTML = 'You must check the box in order to sign up';
+    errorElement.classList.remove('d-none');
+  }
 };
 
 const clearError = () => {
@@ -43,4 +49,5 @@ const clearError = () => {
 form.addEventListener('submit', registerUser);
 emailInput.addEventListener('input', clearError);
 passwordInput.addEventListener('input', clearError);
+checkboxInput.addEventListener('input', clearError);
 loginLink.addEventListener('click', () => { window.location.href = './login.html' })
