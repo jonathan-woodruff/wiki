b1fbf226092e922c469c72be67c093368aece5ce
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-10-20 06:20:47 -0500

    removed comments client src api

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -15,10 +15,6 @@ export async function onLogout() {
     return await axios.get(`${SERVER_URL}/auth/logout`);
 };
 
-export async function checkProtected() {
-    //return await axios.get(`${SERVER_URL}/auth/protected`);
-};
-
 export async function checkForCookie() {
     return await axios.get(`${SERVER_URL}/auth/checkForCookie`);
 };
@@ -26,10 +22,6 @@ export async function checkForCookie() {
 export async function putPassword(payload) {
     return await axios.put(`${SERVER_URL}/auth/changePassword`, payload);
 };
-
-/*export async function putUserName(payload) {
-    return await axios.put(`${SERVER_URL}/auth/updateUserName`, payload);
-};*/
 
 export async function putEmail(payload) {
     return await axios.put(`${SERVER_URL}/auth/updateUserEmail`, payload);
--- client/src/api/main.js
+++ client/src/api/main.js
@@ -19,10 +19,6 @@ export async function putProfile(profileData) {
     return await axios.put(`${SERVER_URL}/main/updateProfile`, profileData);
 };
 
-/*export async function postAvatar(payload) {
-    return await axios.post(`${SERVER_URL}/main/postAvatar`, payload);
-};*/
-
 export async function getCreateWikiData() {
     return await axios.get(`${SERVER_URL}/main/getCreateWikiData`);
 };
