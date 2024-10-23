9065409da113514d2635168843f6463f26692475
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-10-23 14:26:46 -0500

    using custom api subdomain for server url

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'https://wiki-web-service.onrender.com'; //'http://localhost:8000';
-export const CLIENT_URL = 'https://peacechickens.com'; //'http://localhost:8080';
+export const SERVER_URL = 'https://api.peacechickens.com'; //'http://localhost:8000';
+export const CLIENT_URL = 'https://www.peacechickens.com'; //'http://localhost:8080';
 
 export const STRIPE_KEY = 'pk_test_51KvnDxQKwyW3kPTWa3Ng76cvf0duAnAtDB237GuVfr1xvlaRowgh7lfvER6e7afKJptiBog7UfnWc1ZUrGl5kvCq00BROAeKp5';
