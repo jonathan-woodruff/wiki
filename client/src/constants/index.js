5620ddd349c200566d2466ac80414b070f29bd83
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2025-09-17 09:42:18 -0500

    updated url to free version

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,6 +1,6 @@
 const isProduction = true;
 
-export const SERVER_URL = isProduction ? 'https://api.peacechickens.com' : 'http://localhost:8000';
-export const CLIENT_URL = isProduction ? 'https://www.peacechickens.com' : 'http://localhost:8080';
+export const SERVER_URL = isProduction ? 'https://wiki-web-service.onrender.com' : 'http://localhost:8000';
+export const CLIENT_URL = isProduction ? 'https://www.wiki-client.onrender.com' : 'http://localhost:8080';
 
 export const STRIPE_KEY = isProduction ? 'pk_live_51KvnDxQKwyW3kPTW1OaqSCK0O7KO3F6afYI2Dq8bkVJFYMfpiCosl8l8HXMl8bs6Swv6S1EUoQBPfsGmRrt0x2pB00C7zAaB1I' : 'pk_test_51KvnDxQKwyW3kPTWa3Ng76cvf0duAnAtDB237GuVfr1xvlaRowgh7lfvER6e7afKJptiBog7UfnWc1ZUrGl5kvCq00BROAeKp5';
